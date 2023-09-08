723f07fb7bab615ea9f112c1cda33f7dc2cdc16d
afaq0111 <144364614+afaq0111@users.noreply.github.com>
2023-09-08 09:48:01 -0600

    Create README.m

---
+++ README.m
@@ -0,0 +1,6 @@
+- 👋 Hi, I’m @afaq0111
+- 👀 I’m interested in Web Developer 
+- 🌱 I’m currently learning coding
+<!---
+afaq0111/afaq0111 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
